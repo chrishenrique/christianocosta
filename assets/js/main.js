3d8dd636ff57f71e1d53cc86bc5f234d91f4fe20
Christiano Costa <chrishenrique16@hotmail.com>
2019-01-14 13:14:08 -0200

    Make Stars Rating of my skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,6 @@ $(document).ready(function () {
     };
 
 
-
-    // Progress Bar
-    var els = $(".progress-bar");
-    for (i = 0; i < els.length; i++) { 
-        var el = $(els[i]);
-
-        el.animate({
-            width: el.data('valuenow')+'%'
-        }, 2000 );
-    }
-
     // Flag
     var $flagBr = $('#flag-brasil').find('img');
     var $flagUs = $('#flag-usa').find('img');
