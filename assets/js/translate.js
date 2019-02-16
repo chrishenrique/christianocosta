2ef7fa793434d9666686786fd8f48787e9576822
Christiano Costa <chrishenrique16@hotmail.com>
2019-02-16 15:02:20 -0200

    Text and updates

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -23,10 +23,31 @@ function translate(type) {
 function getText(type) {
     var text = {
         'bra': {
-            hi: 'Oi'
+            principal: 'Olá, meu nome é Christiano Costa, sou desenvolvedor fullstack e designer de interface com base no Brasil.',
+            text1: 'Eu sou um sonhador e fanático por todas as coisas digitais. Tenho muitos anos de experiência em desenvolvimento,  amo design minimalista e café.',
+            text2: 'Atualmente com dezenas de sites e sistemas desenvolvidos, clientes nacionais e internacionais que confiaram em mim para o design, implementação e gerenciamento de seus produtos digitais.',
+            experience: 'Experiências Profissionais',
+            actual: 'atual',
+            fullstackdev: 'Desenvolvedor full-stack',
+            traineedev: 'Estagiário em desenvolvimento',
+            academic: 'Acadêmico',
+            skills: 'Conhecimentos',
+            text3: 'Eu bebo café, eu como pizza. Eu escrevo códigos e aprimoro meu design e minhas habilidades todos os dias. Tenho a honra de trabalhar com pessoas especiais.',
+            softdevelop: 'Desenvolvimento de Software',
+            mobileapps: 'Aplicativos Mobile',
+            requirements: 'Análise de Requisitos',
+            devtecnics: 'Técnicas de desenvolvimento agil',
+            english: 'Ingês',
+            basic: 'básico',
+            myportfolio: 'Meu Portifolio',
+            text4: 'Eu bebo café, eu como pizza. Eu escrevo códigos e aprimoro meu design e minhas habilidades todos os dias. Tenho a honra de trabalhar com pessoas especiais.',
+            seemore: 'veja mais',
+
+
         },
         'usa': {
-            hi: 'Hi'
+            hi: 'Hi',
+            myPortfolio: 'My Works'
         },
     };
     return text[type];
