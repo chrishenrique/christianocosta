6e671b38a3b46d347ab1efca209f3c6d21020141
Christiano Costa <chrishenrique16@hotmail.com>
2020-12-03 21:23:44 -0300

    Update site

---
+++ assets/js/dictionary.js
@@ -0,0 +1,281 @@
+var dict = {
+    home: {
+        pt: 'Home',
+        en: 'Home',
+    },
+    about: {
+        pt: 'Sobre',
+        en: 'About',
+    },
+    skills: {
+        pt: 'Conhecimentos',
+        en: 'Skills',
+    },
+    academy: {
+        pt: 'Formação',
+        en: 'Academy',
+    },
+    projects: {
+        pt: 'Projetos',
+        en: 'Projects',
+    },
+    contact: {
+        pt: 'Contato',
+        en: 'Contact',
+    },
+    open: {
+        pt: 'Abrir',
+        en: 'Open',
+    },
+    close: {
+        pt: 'Fechar',
+        en: 'Close',
+    },
+    viewskills: {
+        pt: 'ver habilidades',
+        en: 'view skills',
+    },
+	fullstack: {
+		pt: "Olá, meu nome é Christiano Costa, sou desenvolvedor fullstack e web designer e freelancer.",
+		en: "Hi, my name is Christiano Costa, I am a fullstack developer and web designer."
+    },
+    minimalismtitle: {
+        pt: 'Minimalista',
+        en: 'Minimalist',
+    },
+	minimalism: {
+		pt: "Eu sou um sonhador e fanático por todas as coisas digitais. Tenho muitos anos de experiência em desenvolvimento, amo design minimalista e café.",
+		en: "I am a dreamer and fanatic about all things digital. I have many years of development experience, love minimalist design and coffee."
+	},
+	frontend: {
+		pt: "Dou vida aos dados através do uso de HTML, CSS e JavaScript, para que os usuários possam visualizar e interagir com esses dados. Minha missão é projetar, construir e otimizar interfaces de projetos.",
+		en: "I bring data to life through the use of HTML, CSS and JavaScript, so that users can view and interact with that data. My mission is to design, build and optimize project interfaces."
+    },
+    backend: {
+        pt: 'Crio a lógica para fazer o aplicativo da web funcionar corretamente e consigo isso por meio do uso de linguagens de script do lado do servidor como o PHP.<br>Sei como tornar os aplicativos da web funcionais, sou responsável ​​por otimizar o aplicativo em velocidade e eficiência. Além disso, crio soluções de armazenamento em bancos de dados como MySql, PostgreSQL, SqLite.',
+        en: 'I create the logic to make the web application work correctly and I do it through the use of server-side scripting languages like PHP. <br> I know how to make web applications functional, I am responsible for optimizing the application in speed and efficiency. In addition, I create storage solutions in databases like MySql, PostgreSQL, SqLite.',
+    },
+    webdesign: {
+        pt: 'Minha melhor parte do desenvolvimento é o processo de criação de sites, abranger vários aspectos diferentes, incluindo layout de página da Web/app, produção de conteúdo e design gráfico.',
+        en: 'My best part of the development is the website creation process, covering several different aspects, including webpage / app layout, content production and graphic design.',
+    },
+    uiux: {
+        pt: 'Também sou consumidor de muitos sites e apps, e quado me deparo com algum que não foi bem pensado tanto na visualização, na utilização e na interação, penso em nunca mais voltar... Lua me aguarde!!!',
+        en: 'I am also a consumer of many sites and apps, and when I come across one that was not well thought out in terms of visualization, use and interaction, I think of never returning ... Moon wait for me!!!',
+    },
+    photographytitle: {
+        pt: 'Fotografia',
+        en: 'Photography',
+    },
+    photography: {
+        pt: 'Tirar uma foto é como apertar o botão de pause na vida.',
+        en: 'Taking a photo is like pressing the pause button in life.',
+    },
+    freelancer: {
+        pt: 'Atualmente com dezenas de sites e sistemas desenvolvidos, clientes nacionais e internacionais que confiaram em mim para o design, implementação e gerenciamento de seus produtos digitais.',
+        en: 'Currently with dozens of websites and systems developed, national and international clients have trusted me for the design, implementation and management of their digital products.',
+    },
+    music: {
+        pt: 'Eu acredito que todo guitarrista tem algo de único em sua forma de tocar. Eles só precisam identificar o que os torna diferentes e desenvolver isso.',
+        en: 'I believe every guitar player inherently has something unique about their playing. They just have to identify what makes them different and develop it.',
+    },
+    person: {
+        pt: 'Pessoal',
+        en: 'Person',
+    },
+    aboutme: {
+        pt: 'Sobre mim',
+        en: 'About me',
+    },
+    myskills: {
+        pt: 'Meus conhecimentos',
+        en: 'My skills',
+    },
+    think: {
+        pt: 'Pense',
+        en: 'Think',
+    },
+    jobs: {
+        pt: 'Trabalhos',
+        en: 'jobs',
+    },
+    view: {
+        pt: 'Visão',
+        en: 'View',
+    },
+    myhobbies: {
+        pt: 'Meus hábitos',
+        en: 'My hobbies',
+    },
+    viewinstagram: {
+        pt: 'Veja meu Instagram',
+        en: 'View my Instagram',
+    },
+    musictitle: {
+        pt: 'Musica',
+        en: 'Music',
+    },
+    yearsExperience: {
+        pt: 'ANOS<br> DE EXPERIÊNCIA NA WEB',
+        en: 'YEARS<br> OF EXPERIENCE IN THE WEB',
+    },
+    hello: {
+        pt: 'Olá! Sou o Chris',
+        en: "Hello! I'm Chris",
+    },
+    helloText: {
+        pt: 'Gosto de criar coisas que vivem na Internet, sejam sites, aplicativos ou qualquer coisa entre eles. Meu objetivo é sempre construir produtos que forneçam experiências perfeitas e de alto desempenho..',
+        en: 'I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences.',
+    },
+    helloText2: {
+        pt: 'Sou especializado em desenvolvimento web, ofereço um serviço completo de construção de sites e sistemas online. Trabalho como freelancer ou em conjunto com agências e empresas para dar vida ao seu projeto.',
+        en: 'I am specialized in web development, I offer a complete service of building websites and online systems. I work as a freelancer or in conjunction with agencies and companies to bring your project to life.',
+    },
+    iMakeWebsites: {
+        pt: 'Eu faço websites',
+        en: 'I make websites',
+    },
+    mySkills: {
+        pt: 'Minhas habilidades',
+        en: 'My Skills',
+    },
+    more: {
+        pt: 'Mais',
+        en: 'More',
+    },
+    communication: {
+        pt: 'Comunicação',
+        en: 'Communication',
+    },
+    leadership: {
+        pt: 'Liderança',
+        en: 'Leadership',
+    },
+    flexibility: {
+        pt: 'Flexibilidade e resiliência',
+        en: 'Flexibility and resilience',
+    },
+    teamwork: {
+        pt: 'Trabalho em equipe',
+        en: 'Teamwork',
+    },
+    creativity: {
+        pt: 'Criatividade',
+        en: 'Creativity',
+    },
+    proactivity: {
+        pt: 'Proatividade',
+        en: 'Proactivity',
+    },
+    emphaty: {
+        pt: 'Empatia',
+        en: 'Empathy',
+    },
+    ethics: {
+        pt: 'Ética no trabalho',
+        en: 'Ethics at work',
+    },
+    aprimorate: {
+        pt: 'Eu aprimoro meu código, meu design e minhas habilidades todos os dias.',
+        en: 'I improve my code, my design and my skills every day',
+    },
+    myWork: {
+        pt: 'Meus Trabalhos',
+        en: 'My Work',
+    },
+    englishBasic: {
+        pt: 'Ingês (básico)',
+        en: 'English basic',
+    },
+    worksCharacteristics: {
+        pt: 'Criatividade Design Performance',
+        en: 'Creativity Design Performance',
+    },
+    viewProject: {
+        pt: 'Ver Projeto',
+        en: 'View Project',
+    },
+    workTogether: {
+        pt: 'Vamos trabalhar juntos.',
+        en: "Let's work together.",
+    },
+    workTogetherText: {
+        pt: 'Acredito fortemente que designers e desenvolvedores têm a responsabilidade de garantir que o que estamos construindo não cause danos e tento ser o mais ético possível ao assumir projetos. <br><br>Se você está procurando ajuda para construir algo que promova a sustentabilidade, a diversidade, ou que geralmente tenha como objetivo ter um impacto positivo, então vamos conversar.',
+        en: "I strongly believe that designers and developers have a responsibility to ensure that what we are building does not cause harm and I try to be as ethical as possible when taking on projects.<br><br>If you are looking for help to build something that promotes sustainability, diversity, or that generally aims to have a positive impact, then let's talk",
+    },
+    workTogetherText2: {
+        pt: '',
+        en: '',
+    },
+    feedbackClients: {
+        pt: 'Feedback de clientes.',
+        en: 'Feedback from clients.',
+    },
+    feedback1: {
+        pt: '',
+        en: '"Stage direction, music, ballet, workshops, backstage, the GTG presents the first episode of the series “Les Indes galantes en création"',
+    },
+    feedbackclient1: {
+        pt: '',
+        en: '- Helen Ginsberg, CEO of Treasure Island',
+    },
+    hiring: {
+        pt: 'Gostaria de me contratar.',
+        en: 'I would like to hire me.',
+    },
+    hiring2: {
+        pt: 'Não perca tempo, entre em contato imediatamente!',
+        en: "Don't waste time, contact us immediately!",
+    },
+    location: {
+        pt: 'Localização',
+        en: 'Location',
+    },
+    contact: {
+        pt: 'Contatos',
+        en: 'Contacts',
+    },
+    navigation: {
+        pt: 'Navegação',
+        en: 'Navigation',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    },
+    "": {
+        pt: '',
+        en: '',
+    }
+	
+
+
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,25 @@
+
 $(document).ready(function () {
+
+    // Jquery Translate
+    // var translator = $('body').translate({lang: "pt", t: dict});
+    $('.remove').remove();
+
     // Flag
-    var $flagBr = $('#flag-brasil').find('img');
-    var $flagUs = $('#flag-usa').find('img');
-    $flagBr.click(function(ev){
+    var $flagBr = $('#flag-brasil');
+    var $flagUs = $('#flag-usa');
+    $flagBr.on('click',function(ev){
         ev.preventDefault();
-        $flagUs.removeClass('flag-active');
-        $flagBr.addClass('flag-active');
+        translator.lang("pt");
+        $flagUs.find('img').removeClass('flag-active');
+        $flagBr.find('img').addClass('flag-active');
     });
 
     $flagUs.click(function(ev){
         ev.preventDefault();
-        $flagUs.addClass('flag-active');
-        $flagBr.removeClass('flag-active');
+        translator.lang("en");
+        $flagUs.find('img').addClass('flag-active');
+        $flagBr.find('img').removeClass('flag-active');
     });
     
 
